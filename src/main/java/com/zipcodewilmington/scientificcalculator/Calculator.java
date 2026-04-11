b2c5ca2a9fe86ad92ebb409793d9b4fbbcebf3ed
thejoeflynn <thejoeflynn@gmail.com>
2026-04-11 12:16:54 -0400

    Default state added

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class Calculator {
+    private double currentValue;
+    public Calculator() {
+        this.currentValue = 0.0;
+    }
+
+}
+
