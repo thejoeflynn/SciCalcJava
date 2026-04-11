69bafc11c74a0ffd3f085ac66cb463129caf9a96
thejoeflynn <thejoeflynn@gmail.com>
2026-04-11 13:05:07 -0400

    added core features (add, subtract, etc.)

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -16,5 +16,55 @@ public class Calculator {
     public void changeNumber(double newValue) {
         currentValue = newValue;
     }
+
+    public void add(double value) {
+        currentValue += value;
+    }
+
+    public void subtract(double value) {
+        currentValue -= value;
+    }
+
+    public void multiply(double value) {
+        currentValue *= value;
+    }
+
+    public void divide(double value) {
+        if (value != 0) {
+            currentValue /= value;
+        } else {
+            throw new IllegalArgumentException("ERROR");
+        }
+    }
+
+    public void exponentiate(double exponent) {
+        currentValue = Math.pow(currentValue, exponent);
+    }
+
+    public void square() {
+        currentValue = Math.pow(currentValue, 2);
+    }
+
+    public void squareRoot() {
+        if (currentValue >= 0) {
+            currentValue = Math.sqrt(currentValue);
+        } else {
+            throw new IllegalArgumentException("ERROR");
+        }
+    }
+
+    public void invertSign() {
+        currentValue = -currentValue;
+    }
+
+    public void inverse() {
+        if (currentValue != 0) {
+            currentValue = 1 / currentValue;
+        } else {
+            throw new IllegalArgumentException("ERROR");
+        }
+    }
+
+
 }
 
