cd6f09482f9a74422761025b5d3ba9293b3d0cfd
thejoeflynn <thejoeflynn@gmail.com>
2026-04-11 14:52:01 -0400

    Added Error capability

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -2,8 +2,19 @@ package com.zipcodewilmington.scientificcalculator;
 
 public class Calculator {
     private double currentValue;
+    private boolean isError;
+
     public Calculator() {
         this.currentValue = 0.0;
+        this.isError = false;
+    }
+
+    public String getDisplay() {
+        if (isError) {
+            return "ERROR";
+        } else {
+            return String.valueOf(currentValue);
+        }
     }
 
     public double getCurrentValue() {
@@ -12,56 +23,87 @@ public class Calculator {
 
     public void clearDisplay() {
         currentValue = 0.0;
+        isError = false;
     }
     public void changeNumber(double newValue) {
+        if (isError) {
+            return;
+        }
         currentValue = newValue;
     }
 
     public void add(double value) {
+        if (isError) {
+            return;
+        }
         currentValue += value;
     }
 
     public void subtract(double value) {
+        if (isError) {
+            return;
+        }
         currentValue -= value;
     }
 
     public void multiply(double value) {
+        if (isError) {
+            return;
+        }
         currentValue *= value;
     }
 
     public void divide(double value) {
+        if (isError) {
+            return;
+        }
         if (value != 0) {
             currentValue /= value;
         } else {
-            throw new IllegalArgumentException("ERROR");
+            isError = true;
         }
     }
 
     public void exponentiate(double exponent) {
+        if (isError) {
+            return;
+        }
         currentValue = Math.pow(currentValue, exponent);
     }
 
     public void square() {
+        if (isError) {
+            return;
+        }
         currentValue = Math.pow(currentValue, 2);
     }
 
     public void squareRoot() {
+        if (isError) {
+            return;
+        }
         if (currentValue >= 0) {
             currentValue = Math.sqrt(currentValue);
         } else {
-            throw new IllegalArgumentException("ERROR");
+            isError = true;
         }
     }
 
     public void invertSign() {
+        if (isError) {
+            return;
+        }
         currentValue = -currentValue;
     }
 
     public void inverse() {
+        if (isError) {
+            return;
+        }
         if (currentValue != 0) {
             currentValue = 1 / currentValue;
         } else {
-            throw new IllegalArgumentException("ERROR");
+            isError = true;
         }
     }
 
