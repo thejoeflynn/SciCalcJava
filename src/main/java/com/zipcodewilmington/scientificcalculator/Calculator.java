4cb99d82a652e21d47a2bf4d2de61f87e0d066f3
thejoeflynn <thejoeflynn@gmail.com>
2026-04-11 12:47:26 -0400

    Fully setup the default state and changing the number that is displayed

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -6,5 +6,15 @@ public class Calculator {
         this.currentValue = 0.0;
     }
 
+    public double getCurrentValue() {
+        return currentValue;
+    }
+
+    public void clearDisplay() {
+        currentValue = 0.0;
+    }
+    public void changeNumber(double newValue) {
+        currentValue = newValue;
+    }
 }
 
